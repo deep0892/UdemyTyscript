5359e7225c873360cef47bfbff50ff699106ef58
Deepankar <deepankar080892@gmail.com>
2020-05-26 18:24:48 +0200

    Function as types

--- TypeScript_Basics_and_Basic_Types/app.ts
+++ TypeScript_Basics_and_Basic_Types/app.ts
@@ -13,3 +13,17 @@ function printResult(num: number): void {
 console.log(printResult(add(5, 12)));
 
 // let someValue: undefined;
+
+let combineValues: Function;
+combineValues = add;
+console.log(combineValues(8, 8));
+// combineValues = 5; // Gives error as combineValues is of type function
+combineValues = printResult; // Does not give an error as both are of type function
+console.log(combineValues(add(8, 8)));
+
+// newFunction is a function that accepts two parameters of type
+// numbers and has a return of type number
+let newFunction: (a: number, b: number) => number;
+newFunction = add;
+console.log(newFunction(8, 8));
+// newFunction = printResult; Gives error becoz printResult doesn't satisfy the function type
