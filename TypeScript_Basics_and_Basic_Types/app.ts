eda584ca2af584cc97dae264f33e0ee132a91784
Deepankar <deepankar080892@gmail.com>
2020-05-26 18:30:30 +0200

    Function types and callbacks

--- TypeScript_Basics_and_Basic_Types/app.ts
+++ TypeScript_Basics_and_Basic_Types/app.ts
@@ -27,3 +27,12 @@ let newFunction: (a: number, b: number) => number;
 newFunction = add;
 console.log(newFunction(8, 8));
 // newFunction = printResult; Gives error becoz printResult doesn't satisfy the function type
+
+function addAndhandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndhandle(10, 20, (result) => {
+  console.log(result);
+});
