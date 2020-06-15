d6137a6db75d6ec1d61ab9050e81aae1f94128c3
Deepankar <deepankar080892@gmail.com>
2020-06-15 14:37:17 +0200

    protected properties

--- Understanding-TS/src/app.ts
+++ Understanding-TS/src/app.ts
@@ -1,7 +1,8 @@
 class Department {
   // private readonly id: string;
   // public name: string;
-  private employees: string[] = [];
+  // Protected properties can be accessed from child classes
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -56,6 +57,12 @@ class AccountingDepartmnent extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(name: string) {
+    if (name != 'Max') {
+      this.employees.push(name);
+    }
+  }
+
   addReport(report: string) {
     this.reports.push(report);
   }
@@ -68,5 +75,9 @@ class AccountingDepartmnent extends Department {
 const newAccounting = new AccountingDepartmnent('d3', []);
 newAccounting.addReport('First Report');
 newAccounting.addReport('Second Report');
+newAccounting.addEmployee('Max');
+newAccounting.addEmployee('Mann');
 console.log(newAccounting);
 console.log(newAccounting.printReports());
+console.log(newAccounting.printEmployeeInformation());
+
