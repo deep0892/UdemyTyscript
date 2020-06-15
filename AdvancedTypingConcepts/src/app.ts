2c11a1d862e40251518ea9a3ea433245a641767a
Deepankar <deepankar080892@gmail.com>
2020-06-15 20:26:10 +0200

    Discriminated Union

--- AdvancedTypingConcepts/src/app.ts
+++ AdvancedTypingConcepts/src/app.ts
@@ -74,3 +74,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated Union
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log('Moving with speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 20 });
+moveAnimal({ type: 'horse', runningSpeed: 200 });
