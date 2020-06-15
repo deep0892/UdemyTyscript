c7d9c299288df5e90d55dd04f2fde23aeeaf03fa
Deepankar <deepankar080892@gmail.com>
2020-06-15 20:19:08 +0200

    Type Gaurds

--- AdvancedTypingConcepts/src/app.ts
+++ AdvancedTypingConcepts/src/app.ts
@@ -11,7 +11,7 @@ type Employee = {
 // Intersection types
 type ElevatedEmployee = Admin & Employee;
 
-const el: ElevatedEmployee = {
+const e: ElevatedEmployee = {
   name: 'Max',
   privileges: ['create-server'],
   startDate: new Date()
@@ -22,4 +22,55 @@ type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
 
+// Type gaurds
+function add(a: Combinable, b: Combinable) {
+  // Type gaurd
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
 
+type UnknowEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknowEmployee) {
+  console.log(`Name: ${emp.name}`);
+  if ('privileges' in emp) {
+    console.log(`Priveleges: ${emp.privileges}`);
+  }
+  if ('startDate' in emp) {
+    console.log(`StartDate: ${emp.startDate}`);
+  }
+}
+
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log('Driving.....');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving Truck.....');
+  }
+  loadCargo(amount: number) {
+    console.log(`Loading cargo... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
