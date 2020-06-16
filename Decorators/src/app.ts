96dcb8b516978f97651914c4733c1ccd0f2a7a2d
Deepankar <deepankar080892@gmail.com>
2020-06-16 21:07:10 +0200

    Accessor, Method, Parameter Decorator

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -40,11 +40,36 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+// Accessor Decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+// Method Decorator
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Method Decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+// Parameter Decorator
+function Log4(target: any, name: string | Symbol, positsion: number) {
+  console.log('Parameter Decorator');
+  console.log(target);
+  console.log(name);
+  console.log(positsion);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -56,8 +81,9 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
-
 }
+
