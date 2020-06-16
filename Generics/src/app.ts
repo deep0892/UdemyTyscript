dc8f79af073b84b499ffc3253a7613dc38fc3678
Deepankar <deepankar080892@gmail.com>
2020-06-16 15:45:03 +0200

    Generic Classes

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -48,3 +48,33 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 // Givens error as the object doesnt have name property
 // console.log(extractAndConvert({}, 'name'));
 console.log(extractAndConvert({ name: 'Dips' }, 'name'));
+
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('first');
+textStorage.addItem('second');
+textStorage.removeItem('second');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.removeItem(2);
+console.log('numberStorage', numberStorage.getItems());
+
