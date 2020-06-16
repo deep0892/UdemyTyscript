79f813db3a7f8f67fa008dca65bd3480446d4d88
Deepankar <deepankar080892@gmail.com>
2020-06-16 19:00:35 +0200

    Adding miltiple decorators

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -5,7 +5,20 @@ function Logger(logstring: string) {
   };
 }
 
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    console.log('rendering template');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
 @Logger('LOGGING - Person')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Max';
 
