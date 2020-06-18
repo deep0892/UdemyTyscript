23c633e34302a3432f3b52a482f0ae0dab413f53
Deepankar <deepankar080892@gmail.com>
2020-06-18 21:46:05 +0200

    Refractor import in namespace

--- Writing_Modukle_Code/src/app.ts
+++ Writing_Modukle_Code/src/app.ts
@@ -1,15 +1,7 @@
-/// <reference path="./models/drag-drop.ts"/>
-/// <reference path="./models/project.ts"/>
-/// <reference path="./state/project.ts"/>
-/// <reference path="./util/validation.ts"/>
-/// <reference path="./decorators/autobind.ts"/>
-/// <reference path="./components/base-component.ts"/>
-/// <reference path="./components/project-item.ts"/>
 /// <reference path="./components/project-list.ts"/>
 /// <reference path="./components/project-input.ts"/>
 
 namespace App {
-
   new ProjectInput();
   new ProjectList('active');
   const finishedPrjList = new ProjectList('finished');
--- Writing_Modukle_Code/src/components/project-list.ts
+++ Writing_Modukle_Code/src/components/project-list.ts
@@ -1,3 +1,8 @@
+/// <reference path="./base-component.ts"/> 
+/// <reference path="../decorators/autobind.ts"/>
+/// <reference path="../models/project.ts"/>
+/// <reference path="../models/drag-drop.ts"/>
+/// <reference path="../state/project.ts"/>
 namespace App {
   export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
     assignedProjects: Project[];
