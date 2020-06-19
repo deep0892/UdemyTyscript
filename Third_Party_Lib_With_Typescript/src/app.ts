8ee9fe45b590484985d2c94747f05d1d6a697ce9
Deepankar <deepankar080892@gmail.com>
2020-06-19 12:42:08 +0200

    class-validator

--- Third_Party_Lib_With_Typescript/src/app.ts
+++ Third_Party_Lib_With_Typescript/src/app.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 import { Product } from './product.model';
+
 
 const products = [{ title: 'A book', price: 12.80 }, { title: 'A Carpet', price: 10.80 }];
 // const p1 = new Product('A Book', 12.99);
@@ -14,3 +16,13 @@ const loadedProducts = plainToClass(Product, products);
 for (const prod of loadedProducts) {
   console.log(prod.getInformation());
 }
+
+const newProd = new Product('', -19.99);
+
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
