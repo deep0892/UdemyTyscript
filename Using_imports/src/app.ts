c77022b30604a3b1e5d524ae66260b89c43b57c8
Deepankar <deepankar080892@gmail.com>
2020-06-19 11:32:07 +0200

    Webpack setup

--- Using_imports/src/app.ts
+++ Using_imports/src/app.ts
@@ -1,5 +1,3 @@
-/// <reference path="./components/project-list.ts"/>
-/// <reference path="./components/project-input.ts"/>
 import { ProjectInput } from './components/project-input.js';
 import { ProjectList } from './components/project-list.js';
 
---
+++ Webpack_Setup/src/app.ts
@@ -0,0 +1,6 @@
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
+
+new ProjectInput();
+new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
